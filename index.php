8fe96ce66fd073023ca8b25b4c6b2b813e4f5d00
Henrico Queiroz <112563903+HenricQ@users.noreply.github.com>
2025-12-04 20:58:04 -0400

    if it works it works

--- index.php
+++ index.php
@@ -45,11 +45,38 @@ switch ($page) {
     case 'admin_places':
         (new AdminController())->places();
         break;
+    case 'admin_place_new':
+        (new AdminController())->newPlace();
+        break;
+    case 'admin_place_create':
+        (new AdminController())->createPlace();
+        break;
+    case 'admin_place_edit':
+        (new AdminController())->editPlace();
+        break;
+    case 'admin_place_update':
+        (new AdminController())->updatePlace();
+        break;
+    case 'admin_place_delete':
+        (new AdminController())->deletePlace();
+        break;
     case 'admin_rewards':
         (new AdminController())->rewards();
         break;
-    case 'admin_reviews':
-        (new AdminController())->reviews();
+    case 'admin_reward_new':
+        (new AdminController())->newReward();
+        break;
+    case 'admin_reward_create':
+        (new AdminController())->createReward();
+        break;
+    case 'admin_reward_edit':
+        (new AdminController())->editReward();
+        break;
+    case 'admin_reward_update':
+        (new AdminController())->updateReward();
+        break;
+    case 'admin_reward_delete':
+        (new AdminController())->deleteReward();
         break;
     case 'profile':
         (new ProfileController())->show();
--- views/admin/rewards.php
+++ views/admin/rewards.php
@@ -2,7 +2,7 @@
 
 <div class="d-flex justify-content-between align-items-center mb-3">
   <h3>Gerenciar Recompensas</h3>
-  <a href="#" class="btn btn-success">Nova Recompensa</a>
+  <a href="index.php?page=admin_reward_new" class="btn btn-success">Nova Recompensa</a>
 </div>
 
 <table class="table table-striped">
@@ -10,11 +10,15 @@
   <tbody>
     <?php foreach ($rewards as $r): ?>
       <tr>
-        <td><?php echo $r['name']; ?></td>
-        <td><?php echo $r['points_required']; ?></td>
+        <td><?php echo htmlspecialchars($r['name']); ?></td>
+        <td><?php echo htmlspecialchars($r['points_required']); ?></td>
         <td>
-          <a class="btn btn-sm btn-primary" href="#">Editar</a>
-          <a class="btn btn-sm btn-danger" href="#">Excluir</a>
+          <a class="btn btn-sm btn-primary" href="index.php?page=admin_reward_edit&id=<?php echo urlencode($r['id']); ?>">Editar</a>
+
+          <form method="post" action="index.php?page=admin_reward_delete" style="display:inline-block;margin-left:6px;" onsubmit="return confirm('Deseja excluir esta recompensa?');">
+            <input type="hidden" name="id" value="<?php echo htmlspecialchars($r['id']); ?>">
+            <button class="btn btn-sm btn-danger">Excluir</button>
+          </form>
         </td>
       </tr>
     <?php endforeach; ?>
